37736a74887363bf4af2e99c897ed91e5134c371
Guido De Gobbis <guido@degobbis.de>
2017-01-24 14:59:52 +0100

    Code-Refacoting

--- jtcsv2html.scriptfile.php
+++ jtcsv2html.scriptfile.php
@@ -35,37 +35,35 @@ class PlgSearchJtCsv2htmlInstallerScript
 		// We only need to perform this if the extension is being installed, not updated
 		if ($route == 'install')
 		{
-			$db     = JFactory::getDbo();
-			$query  = $db->getQuery(true);
+			$db = JFactory::getDbo();
 
-			$fields1 = $db->quoteName('enabled') . ' = ' . (int) 1;
-			$fields2 = $db->quoteName('enabled') . ' = ' . (int) 0;
-
-			$conditions1 = array(
-				$db->quoteName('folder') . ' = ' . $db->quote('search'),
-				$db->quoteName('element') . ' = ' . $db->quote('jtcsv2html'),
-				$db->quoteName('type') . ' = ' . $db->quote('plugin')
+			$conditions = array(
+				array(
+					$db->quoteName('enabled') . ' = ' . (int) 1,
+					array(
+						$db->quoteName('folder') . ' = ' . $db->quote('search'),
+						$db->quoteName('element') . ' = ' . $db->quote('jtcsv2html'),
+						$db->quoteName('type') . ' = ' . $db->quote('plugin')
+					)
+				),
+				array(
+					$db->quoteName('enabled') . ' = ' . (int) 0,
+					array(
+						$db->quoteName('folder') . ' = ' . $db->quote('search'),
+						$db->quoteName('element') . ' = ' . $db->quote('content'),
+						$db->quoteName('type') . ' = ' . $db->quote('plugin')
+					)
+				)
 			);
 
-			$conditions2 = array(
-				$db->quoteName('folder') . ' = ' . $db->quote('search'),
-				$db->quoteName('element') . ' = ' . $db->quote('content'),
-				$db->quoteName('type') . ' = ' . $db->quote('plugin')
-			);
-
-			$query->update($db->quoteName('#__extensions'))
-				->set($fields1)->where($conditions1);
-
-			$db->setQuery($query);
-			$db->execute();
-
-			$query1  = $db->getQuery(true);
-
-			$query1->update($db->quoteName('#__extensions'))
-				->set($fields2)->where($conditions2);
-
-			$db->setQuery($query1);
-			$db->execute();
+			foreach ($conditions as $condition)
+			{
+				$query = $db->getQuery(true);
+				$query->update($db->quoteName('#__extensions'))
+					->set($condition[0])->where($condition[1]);
+				$db->setQuery($query);
+				$db->execute();
+			}
 		}
 	}
 
@@ -82,37 +80,35 @@ class PlgSearchJtCsv2htmlInstallerScript
 		// We only need to perform this if the extension is being uninstalled
 		if ($route == 'uninstall')
 		{
-			$db     = JFactory::getDbo();
-			$query  = $db->getQuery(true);
+			$db = JFactory::getDbo();
 
-			$fields1 = $db->quoteName('enabled') . ' = ' . (int) 0;
-			$fields2 = $db->quoteName('enabled') . ' = ' . (int) 1;
-
-			$conditions1 = array(
-				$db->quoteName('folder') . ' = ' . $db->quote('search'),
-				$db->quoteName('element') . ' = ' . $db->quote('jtcsv2html'),
-				$db->quoteName('type') . ' = ' . $db->quote('plugin')
+			$conditions = array(
+				array(
+					$db->quoteName('enabled') . ' = ' . (int) 1,
+					array(
+						$db->quoteName('folder') . ' = ' . $db->quote('search'),
+						$db->quoteName('element') . ' = ' . $db->quote('jtcsv2html'),
+						$db->quoteName('type') . ' = ' . $db->quote('plugin')
+					)
+				),
+				array(
+					$db->quoteName('enabled') . ' = ' . (int) 0,
+					array(
+						$db->quoteName('folder') . ' = ' . $db->quote('search'),
+						$db->quoteName('element') . ' = ' . $db->quote('content'),
+						$db->quoteName('type') . ' = ' . $db->quote('plugin')
+					)
+				)
 			);
 
-			$conditions2 = array(
-				$db->quoteName('folder') . ' = ' . $db->quote('search'),
-				$db->quoteName('element') . ' = ' . $db->quote('content'),
-				$db->quoteName('type') . ' = ' . $db->quote('plugin')
-			);
-
-			$query->update($db->quoteName('#__extensions'))
-				->set($fields1)->where($conditions1);
-
-			$db->setQuery($query);
-			$db->execute();
-
-			$query1  = $db->getQuery(true);
-
-			$query1->update($db->quoteName('#__extensions'))
-				->set($fields2)->where($conditions2);
-
-			$db->setQuery($query1);
-			$db->execute();
+			foreach ($conditions as $condition)
+			{
+				$query = $db->getQuery(true);
+				$query->update($db->quoteName('#__extensions'))
+					->set($condition[0])->where($condition[1]);
+				$db->setQuery($query);
+				$db->execute();
+			}
 		}
 	}
 }
